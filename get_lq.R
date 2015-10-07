536ae676c18ea23bf023122e243878d60aa93f8d
Chun Liu <cl316@saxon.stat.duke.edu>
2015-10-07 09:59:53 -0400

    started get_lq and parse_lq part

---
+++ get_lq.R
@@ -0,0 +1,22 @@
+library(rvest)
+library(stringr)
+library(magrittr)
+
+dir.create("data/lq/main/", recursive = TRUE, showWarnings = FALSE)
+base_url = "http://www.lq.com"
+listing_page = "/en/findandbook/hotel-listings.html"
+
+download.file(paste(base_url, listing_page, sep = ""),
+              destfile = "data/lq/main/listings.html")
+file = read_html("data/lq/main/listings.html")
+hotel_url = html_nodes(file, "#hotelListing .col-sm-12 a") %>%
+  html_attr("href") %>%
+  . [!is.na(.)] %>%
+  paste0(base_url, .) %>%
+  head(n = 5)
+
+for(url in hotel_url){
+  download.file(url, destfile = paste0("data/lq/", basename(url)))
+}
+
+
