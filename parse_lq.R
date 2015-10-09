68070c329d3809113d56323e0d5cd4afdcd2397a
Chun Liu <cl316@saxon.stat.duke.edu>
2015-10-09 19:37:15 -0400

    updated parse_lq

--- parse_lq.R
+++ parse_lq.R
@@ -5,7 +5,18 @@ for(file in files)
 {
   html = read_html(file)
   
-  addr = html_nodes(html, ".hotelDetailsBasicInfoTitle p") %>% html_text()
+  info = html_nodes(html, ".hotelDetailsBasicInfoTitle p") %>%
+         html_text() %>%
+         str_trim() %>%
+         str_replace_all("\n[ ]{2,}","")
+  
+  addr = strsplit(strsplit(strsplit(info, "Phone:")[[1]],"Fax:")[[1]],"Fax:")
+  
+  numbers = str_extract_all(info, "([0-9])[- .]([0-9]{3})[- .]([0-9]{3})[- .]([0-9]{4})")
+  
+  phone = numbers[[1]][1]
+  
+  fax= numbers[[1]][2]
   
   lat_long = html_nodes(html, ".minimap") %>% html_attr("src")
 }
