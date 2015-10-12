a2f2c5f93ad3385268d367c79339a3b2e6093e15
Lei Qian <lei.qian@duke.edu>
2015-10-12 13:11:09 -0400

    changed denny's coords, made write up rmd file, wrote code to parse denny's

---
+++ parse_dennys.R
@@ -0,0 +1,32 @@
+library(rvest)
+files <- dir("data/dennys/",pattern = "*.xml",full.names = TRUE)
+
+mydf <- NULL
+
+for(file in files)
+{
+  xml <- read_xml(file)
+  collection <- xml_nodes(xml, "collection")
+  x <- collection[[1]]
+  df <- data.frame(
+    uid = xml_nodes(x,"uid") %>% xml_text()
+  )
+  
+  df$Name <- xml_nodes(x, "name") %>% xml_text()
+  df$Address1 <- xml_nodes(x,"address1") %>% xml_text()
+  df$Address2 <- xml_nodes(x, "address2") %>% xml_text()
+  df$City <- xml_nodes(x,"city") %>% xml_text()
+  df$State <- xml_nodes(x,"state") %>% xml_text()
+  df$Country <- xml_nodes(x,"country") %>% xml_text()
+  df$PostalCode <- xml_nodes(x,"postalcode") %>% xml_text()
+  df$PhoneNumber <- xml_nodes(x,"phone") %>% xml_text()
+  df$FaxNumber <- xml_nodes(x,"fax") %>% xml_text()
+  df$Latitude <- xml_nodes(x,"latitude") %>% xml_text()
+  df$Longitude <- xml_nodes(x,"longitude") %>% xml_text()
+  
+  mydf <- rbind(mydf, df)
+}
+
+mydf <- unique(mydf)
+mydf <- mydf[mydf$Country == "US",]
+
