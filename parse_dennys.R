43b6e380eefbee5a2c749cdc730a342cdbf20ffa
Lei Qian <lei.qian@duke.edu>
2015-10-12 16:45:45 -0400

    made some additions to parse dennys

--- parse_dennys.R
+++ parse_dennys.R
@@ -12,16 +12,9 @@ for(file in files)
     uid = xml_nodes(x,"uid") %>% xml_text()
   )
   
-
-  df$Name <- xml_nodes(x, "name") %>% xml_text()
-  df$Address1 <- xml_nodes(x,"address1") %>% xml_text()
-  df$Address2 <- xml_nodes(x, "address2") %>% xml_text()
-  df$City <- xml_nodes(x,"city") %>% xml_text()
-  df$State <- xml_nodes(x,"state") %>% xml_text()
   df$Country <- xml_nodes(x,"country") %>% xml_text()
-  df$PostalCode <- xml_nodes(x,"postalcode") %>% xml_text()
-  df$PhoneNumber <- xml_nodes(x,"phone") %>% xml_text()
-  df$FaxNumber <- xml_nodes(x,"fax") %>% xml_text()
+  df$State <- xml_nodes(x,"state") %>% xml_text()
+  df$Address <- xml_nodes(x,"address1") %>% xml_text()
   df$Latitude <- xml_nodes(x,"latitude") %>% xml_text()
   df$Longitude <- xml_nodes(x,"longitude") %>% xml_text()
   
@@ -30,4 +23,7 @@ for(file in files)
 
 mydf <- unique(mydf)
 mydf <- mydf[mydf$Country == "US",]
+
+
+
 
