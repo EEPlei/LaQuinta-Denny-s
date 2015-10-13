de1bb86489988772c4541616f63ea2b44985f783
Lei Qian <lei.qian@duke.edu>
2015-10-13 14:21:39 -0400

    updated

--- parse_lq.R
+++ parse_lq.R
@@ -6,21 +6,21 @@ extract_data = function(files){
                        State = NA, Zipcode = NA, 
                        Phone = NA, Fax = NA, 
                        Latitude = NA, Longitude = NA, stringsAsFactors = FALSE)
- 
-   for(file in files)
+  
+  for(file in files)
   {
-   
+    
     html = read_html(file)
     
     info = html_nodes(html, ".hotelDetailsBasicInfoTitle p") %>%
-           html_text() %>%
-           str_trim() %>%
-           str_replace_all("\n[ ]{2,}","")
+      html_text() %>%
+      str_trim() %>%
+      str_replace_all("\n[ ]{2,}","")
     
     addr = strsplit(strsplit(strsplit(info, "Phone:")[[1]],"Fax:")[[1]],"Fax:")
     addr_parse1 = as.character(unlist(addr))
     addr_parse2 = str_split(addr_parse1,", ")[[1]][3] %>%
-                  str_split(.," ")
+      str_split(.," ")
     numbers = str_extract_all(info, "([0-9])[- .]([0-9]{3})[- .]([0-9]{3})[- .]([0-9]{4})")
     lat_long = html_nodes(html, ".minimap") %>% html_attr("src")
     
@@ -30,15 +30,18 @@ extract_data = function(files){
                addr_parse2[[1]][2],
                numbers[[1]][1],
                numbers[[1]][2],
-               str_extract_all(lat_long,"[0-9-.]{4,}")[[1]][1],
-               str_extract_all(lat_long,"[0-9-.]{4,}")[[1]][2]
-               )
+               str_extract_all(lat_long,"[0-9.-]{4,}")[[1]][1],
+               str_extract_all(lat_long,"[0-9.-]{4,}")[[1]][2]
+    )
     
     lq_data = rbind(lq_data, setNames(as.list(newrow), names(lq_data)))
     lq_final = lq_data[-1,]
   } 
   return(lq_final)
 }
+
+
+
 
 
 
